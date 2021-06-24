712ee47812bbad68d2320c289d048fc95504ea22
janepounds <janepounds1@gmail.com>
2021-06-24 15:53:34 +0300

    added navigations

--- app/src/main/java/com/example/mykotlinapp/MainActivity.kt
+++ app/src/main/java/com/example/mykotlinapp/MainActivity.kt
@@ -1,34 +1,82 @@
 package com.example.mykotlinapp
 
 import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.FragmentContainerView
+import androidx.fragment.app.FragmentManager
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.navigation.ui.NavigationUI
+import com.example.mykotlinapp.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
-    lateinit var bottom_nav: BottomNavigationView
+
     lateinit var fragment_container: FragmentContainerView
     lateinit var navController: NavController
-    lateinit var navHostFragment:NavHostFragment
     lateinit var context:Context
+    lateinit var actionBar: ActionBar
+    lateinit var navHostFragment:NavHostFragment
+    lateinit var binding:ActivityMainBinding
+    lateinit var fragmentManager: FragmentManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        bottom_nav = findViewById(R.id.bottom_navigation)
-        fragment_container = findViewById(R.id.home_container)
-
-        navHostFragment = supportFragmentManager.findFragmentById(R.id.home_container) as NavHostFragment
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        fragment_container = binding.navigationHome
+        fragmentManager = supportFragmentManager
+        navHostFragment = (supportFragmentManager.findFragmentById(binding.navigationHome.id) as NavHostFragment?)!!
         navController = navHostFragment.navController
-
         context = applicationContext
         setUpNavigation()
+        setSupportActionBar(binding.mainToolbar)
+        actionBar = supportActionBar!!
+        actionBar.setDisplayShowTitleEnabled(false)
+        actionBar.setHomeButtonEnabled(false)
+        actionBar.setDisplayHomeAsUpEnabled(false)
+        NavigationUI.setupWithNavController(binding.bottomNavigation, navController);
 
     }
 
     private fun setUpNavigation() {
+        binding.bottomNavigation.itemIconTintList == null
+        binding.bottomNavigation.setOnNavigationItemSelectedListener { item ->
+            var currentDestination: Int
+            when (item.itemId) {
+                R.id.navigation_home -> {
+                    currentDestination = navController.currentDestination!!.id
 
+
+                }
+                R.id.accountFragment2 -> {
+                    currentDestination = navController.currentDestination!!.id
+                    if (currentDestination == R.id.accountFragment2)
+                        navController.navigate(R.id.action_navigation_home_to_accountFragment2)
+                }
+
+
+            }
+            true }
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
+    }
+
+    override fun onBackPressed() {
+        if (navController.currentDestination!!.id!=R.id.navigation_home) {
+            // Pop previous Fragment
+            navController.popBackStack();
+
+        }else{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
