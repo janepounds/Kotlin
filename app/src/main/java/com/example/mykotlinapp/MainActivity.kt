0792654fa502035254b675303a5b24ec46ee8db0
janepounds <janepounds1@gmail.com>
2021-06-24 10:35:46 +0300

    added nav controller

--- app/src/main/java/com/example/mykotlinapp/MainActivity.kt
+++ app/src/main/java/com/example/mykotlinapp/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.example.mykotlinapp
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentContainerView
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var bottom_nav: BottomNavigationView
+    lateinit var fragment_container: FragmentContainerView
+    lateinit var navController: NavController
+    lateinit var navHostFragment:NavHostFragment
+    lateinit var context:Context
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        bottom_nav = findViewById(R.id.bottom_navigation)
+        fragment_container = findViewById(R.id.home_container)
+
+        navHostFragment = supportFragmentManager.findFragmentById(R.id.home_container) as NavHostFragment
+        navController = navHostFragment.navController
+
+        context = applicationContext
+        setUpNavigation()
+
+    }
+
+    private fun setUpNavigation() {
+
     }
 }
